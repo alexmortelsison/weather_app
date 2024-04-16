d210cf67a3e9f1f67c2c4b8c23eb6dce2bf6ae65
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-04-16 10:33:38 +0900

    updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/pages/get-started.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      home: GetStarted(),
+    );
   }
 }
---
+++ lib/pages/get-started.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({super.key});
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
