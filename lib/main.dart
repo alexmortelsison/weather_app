2c67515c8a596db48e8e26283b89d970d2dbe124
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-04-16 11:14:31 +0900

    updatedUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/pages/get-started.dart';
+
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Weather App',
-      home: GetStarted(),
+      home: HomeScreen(),
     );
   }
 }
